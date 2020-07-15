5d1a2910a08ebdeea5434d8737c95c764d47d6b3
Sinisa Vrhovac <sinisa.vrhovac@gmail.com>
2020-07-16 00:41:01 +0200

    Updating project with new features

--- regression-tests/src/test/java/ShopHomePageTests.java
+++ regression-tests/src/test/java/ShopHomePageTests.java
@@ -1,3 +1,5 @@
+import io.qameta.allure.*;
+import io.qameta.allure.junit4.DisplayName;
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 import shop_test.framework.core.BaseTest;
@@ -10,12 +12,10 @@ import shop_test.pageobjects.ShopHomePage;
  */
 public class ShopHomePageTests extends BaseTest {
 
-    private static SoftAssert sa = new SoftAssert();
+    private static final SoftAssert sa = new SoftAssert();
 
     /**
      * Open home page, the URL is specified in Tests.xml as parameter
-     * @version 0.1
-     * @author Sinisa Vrhovac
      *
      * @param URL The page URL to open before each test
      */
@@ -28,13 +28,17 @@ public class ShopHomePageTests extends BaseTest {
     /**
      * Count number of products on home page and validate
      * if matches expected size provided as parameter.
-     * @version 0.1
-     * @author Sinisa Vrhovac
      *
      * @param expectedSize Number of products expected to be displayed on page
      */
     @Test
     @Parameters ("page-size")
+    @DisplayName("Home Page Product Count Test")
+    @Description("Validate that expected number of products is displayed on page")
+    @Epic("TP1-2")
+    @Story("R_001 - Display products on home page")
+    @Link(name = "JIRA Issue TP1-3", url = "https://lighthousetesting.atlassian.net/browse/TP1-3")
+    @Feature("AC02 - Maximum number of products displayed is 20;")
     public void ShopHomePageProductCountTest (int expectedSize) {
         ShopHomePage homePage = new ShopHomePage(getDriver());
         int size = homePage.getHomeProducts().size();
@@ -44,8 +48,6 @@ public class ShopHomePageTests extends BaseTest {
 
     /**
      * Validates that product properties are present on page.
-     * @version 0.1
-     * @author Sinisa Vrhovac
      */
     @Test (dataProvider = "dp-product", dataProviderClass = DProvider.class)
     @Ignore
