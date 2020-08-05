fa5134753717bb38e8dea4b1a70d637d6a121145
vesna357 <vesna9@gmail.com>
2020-08-05 21:22:20 +0200

    izmene u metodama

--- page-objects/src/main/java/shop_test/pageobjects/ShoppingChartPage.java
+++ page-objects/src/main/java/shop_test/pageobjects/ShoppingChartPage.java
@@ -1,60 +1,57 @@
-package shop_test.pageobjects;
+package selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import shop_test.framework.core.BasePage;
+
+import selenium.BasePage;
 
 public class ShoppingChartPage extends BasePage {
 
-		public ShoppingChartPage(final WebDriver driver) {
-			super(driver);
-		}
+	public ShoppingChartPage(WebDriver driver) {
+		super(driver);
 
-		public static By arrowUpBtn = By.xpath("//i[@class='material-icons touchspin-up']");
+	}
 
-		private static By proceedBtn = By.xpath("//a[@class='btn btn-primary']");
+	private static By arrowUpBtn = By.xpath("//i[@class='material-icons touchspin-up']");
 
-		private static By quantity = By.xpath("//input[@name='product-quantity-spin']");
+	private static By proceedBtn = By.xpath("//a[@class='btn btn-primary']");
 
-		private static By deleteBtn = By.xpath("//i[@class='material-icons float-xs-left']");
+	private static By quantity = By.xpath("//input[@name='product-quantity-spin']");
 
+	private static By deleteBtn = By.xpath("//i[@class='material-icons float-xs-left']");
 
+	public WebElement getArrowUpBtn() {
+		waitForElementToAppear(arrowUpBtn);
+		return this.driver.findElement(arrowUpBtn);
+	}
 
-		public WebElement getArrowUpBtn() {
-			waitForElementToAppear(arrowUpBtn);
-			return this.driver.findElement(arrowUpBtn);
-		}
+	public WebElement getProceedBtn() {
+		waitForElementToAppear(proceedBtn);
+		return this.driver.findElement(proceedBtn);
+	}
 
+	public WebElement getQuantity() {
+		waitForElementToAppear(quantity);
+		return this.driver.findElement(quantity);
 
-		public static void setArrowUpBtn(By arrowUpBtn) {
-			ShoppingChartPage.arrowUpBtn = arrowUpBtn;
-		}
+	}
 
+	public WebElement getDeleteBtn() {
+		waitForElementToAppear(deleteBtn);
+		return this.driver.findElement(deleteBtn);
+	}
 
-		public static By getProceedBtn() {
-			return proceedBtn;
-		}
+	public void clickgetArrowUpBtn() {
+		getProceedBtn().click();
+	}
 
+	public void clickProceedBtn() {
+		getProceedBtn().click();
+	}
 
-		public static void setProceedBtn(By proceedBtn) {
-			ShoppingChartPage.proceedBtn = proceedBtn;
-		}
+	public void clickDeleteBtn() {
+		getDeleteBtn().click();
+	}
 
-
-		public static By getQuantity() {
-			return quantity;
-		}
-
-
-		public static void setQuantity(By quantity) {
-			ShoppingChartPage.quantity = quantity;
-		}
-		
-		public static By getDeleteBtn() {
-			return quantity;
-		}
-			}
-
-
-
+}
