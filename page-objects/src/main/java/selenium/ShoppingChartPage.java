d48911d1470ded548beec77ca2edbf02981e448a
vesna357 <vesna9@gmail.com>
2020-08-07 16:33:02 +0200

    popravke posle komentara

--- page-objects/src/main/java/selenium/ShoppingChartPage.java
+++ page-objects/src/main/java/selenium/ShoppingChartPage.java
@@ -13,21 +13,28 @@ public class ShoppingChartPage extends BasePage {
 
 	}
 
-	private static By arrowUpBtn = By.xpath("//button[@class='btn btn-touchspin js-touchspin js-increase-product-quantity bootstrap-touchspin-up']");
+	private static By arrowUpBtn = By.xpath(
+			"//button[@class='btn btn-touchspin js-touchspin js-increase-product-quantity bootstrap-touchspin-up']");
 
-	private static By arrowDwnBtn = By.xpath("//button[@class='btn btn-touchspin js-touchspin js-decrease-product-quantity bootstrap-touchspin-down']");
-	
+	private static By arrowDwnBtn = By.xpath(
+			"//button[@class='btn btn-touchspin js-touchspin js-decrease-product-quantity bootstrap-touchspin-down']");
+
 	private static By proceedBtn = By.xpath("//a[@class='btn btn-primary']");
 
 	private static By quantity = By.xpath("//li[1]//div[1]//div[3]//div[1]//div[2]//div[1]//div[1]//div[1]//input[1]");
 
 	private static By deleteBtn = By.xpath("//a[@class='remove-from-cart']");
 
+	private static By continueBtn = By.xpath(
+			"//body[@id='cart']/main/section[@id='wrapper']/div[@class='container']/div[@id='content-wrapper']/section[@id='main']/div[@class='cart-grid row']/div[@class='cart-grid-body col-xs-12 col-lg-8']/a[1]");
+
+	
+
 	public WebElement getArrowUpBtn() {
 		waitForElementToAppear(arrowUpBtn);
 		return this.driver.findElement(arrowUpBtn);
 	}
-	
+
 	public WebElement getArrowDwnBtn() {
 		waitForElementToAppear(arrowDwnBtn);
 		return this.driver.findElement(arrowDwnBtn);
@@ -49,23 +56,8 @@ public class ShoppingChartPage extends BasePage {
 		return this.driver.findElement(deleteBtn);
 	}
 
-	public void clickArrowUpBtn() {
-		getArrowUpBtn().click();
+	public WebElement getContinueBtn() {
+		waitForElementToAppear(continueBtn);
+		return this.driver.findElement(continueBtn);
 	}
-
-	public void clickArrowDwnBtn() {
-		getArrowDwnBtn().click();
-	}
-
-	
-	public void clickProceedBtn() {
-		getProceedBtn().click();
-	}
-
-	public void clickDeleteBtn() {
-		getDeleteBtn().click();
-	}
-
-	
-
 }
